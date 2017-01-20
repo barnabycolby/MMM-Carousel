94fa2bac700030ceb86e1a2368a519153d9034eb
gordywills <gordy.wills@gmail.com>
2017-01-20 15:42:56 +0000

    refactor to reduce repeated code

--- MMM-Carousel.js
+++ MMM-Carousel.js
@@ -21,29 +21,42 @@
         },
 
         notificationReceived: function (notification) {
-            var i;
             var positions = ['top_bar', 'bottom_bar', 'top_left', 'bottom_left', 'top_center', 'bottom_center', 'top_right', 'bottom_right', 'upper_third', 'middle_center', 'lower_third'];
-            var modules;
 
             if (notification === 'DOM_OBJECTS_CREATED') {
                 // Initially, all modules are hidden except the first and any ignored modules
                 // We start by getting a list of all of the modules in the transition cycle
-                    modules = MM.getModules().exceptModule(this).filter(function (module) {
-                        if (this.config.global === true) {
-                            return this.config.ignoreModules.indexOf(module.name) === -1;
-                        } else {
-                            return ((this.config[positions[position]].ignoreModules.indexOf(module.name) === -1) && (module.data.position === positions[position]));
-
+                if (this.config.global === true) {
+                    this.setUpTransitionTimers(true, null, null);
+                } else {
+                    for (var position in positions) {
+                        if (!positions.hasOwnProperty(position)) continue;
+                        if (!this.config.hasOwnProperty(positions[position])) continue;
+                        if (this.config[positions[position]].enabled === true) {
+                            this.setUpTransitionTimers(false, position[position]);
                         }
-                    }, this);
-                    modules.currentIndex = 0;
-                    for (i = 1; i < this.modules.length; i += 1) {
-                        modules[i].hide();
                     }
-
-                    // We set a timer to cause the page transitions
-                    this.transitionTimer = setInterval(this.moduleTransition.bind(modules), this.config.transitionInterval);
+                }
             }
+        },
+
+        setUpTransitionTimers: function(global, positionIndex) {
+            var i;
+            var modules;
+            modules = MM.getModules().exceptModule(this).filter(function (module) {
+                if (this.config.global === true) {
+                    return this.config.ignoreModules.indexOf(module.name) === -1;
+                } else {
+                    return ((this.config[positionIndex].ignoreModules.indexOf(module.name) === -1) && (module.data.position === positionIndex));
+                }
+            }, this);
+            modules.currentIndex = 0;
+            for (i = 1; i < this.modules.length; i += 1) {
+                modules[i].hide();
+            }
+
+            // We set a timer to cause the page transitions
+            this.transitionTimer = setInterval(this.moduleTransition.bind(modules), this.config.transitionInterval);
         },
 
         moduleTransition: function () {
