25b430372adaaec79ed2577ba14b9d37da1cec37
gordywills <gordy.wills@gmail.com>
2017-01-20 16:42:59 +0000

    fixing errors in the build

--- MMM-Carousel.js
+++ MMM-Carousel.js
@@ -21,16 +21,16 @@
         },
 
         notificationReceived: function (notification) {
-            var i, position, positions = ['top_bar', 'bottom_bar', 'top_left', 'bottom_left', 'top_center', 'bottom_center', 'top_right', 'bottom_right', 'upper_third', 'middle_center', 'lower_third'];
+            var position, positions = ['top_bar', 'bottom_bar', 'top_left', 'bottom_left', 'top_center', 'bottom_center', 'top_right', 'bottom_right', 'upper_third', 'middle_center', 'lower_third'];
             if (notification === 'DOM_OBJECTS_CREATED') {
                 // Initially, all modules are hidden except the first and any ignored modules
                 // We start by getting a list of all of the modules in the transition cycle
                 if (this.config.global === true) {
                     this.setUpTransitionTimers(null);
                 } else {
-                    for (i=0;i < positions.length; i++) {
-                        if (this.config[positions[i]].enabled === true) {
-                            this.setUpTransitionTimers(positions[i]);
+                    for (position=0;position < positions.length; position++) {
+                        if (this.config[positions[position]].enabled === true) {
+                            this.setUpTransitionTimers(positions[position]);
                         }
                     }
                 }
