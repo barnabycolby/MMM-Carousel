c499b9ab52604a9472359fede758ddd85f27bd2e
gordywills <gordy.wills@gmail.com>
2017-01-29 19:08:33 +0000

    refactor to reduce repeated code

--- MMM-Carousel.js
+++ MMM-Carousel.js
@@ -6,18 +6,18 @@
         defaults: {
             transitionInterval: 10000,
             ignoreModules: [],
-            mode: 'global',
-            top_bar: {enabled: false, ignoreModules: [], transitionInterval: 10000},
-            top_left: {enabled: false, ignoreModules: [], transitionInterval: 10000},
-            top_center: {enabled: false, ignoreModules: [], transitionInterval: 10000},
-            top_right: {enabled: false, ignoreModules: [], transitionInterval: 10000},
-            upper_third: {enabled: false, ignoreModules: [], transitionInterval: 10000},
-            middle_center: {enabled: false, ignoreModules: [], transitionInterval: 10000},
-            lower_third: {enabled: false, ignoreModules: [], transitionInterval: 10000},
-            bottom_left: {enabled: false, ignoreModules: [], transitionInterval: 10000},
-            bottom_center: {enabled: false, ignoreModules: [], transitionInterval: 10000},
-            bottom_right: {enabled: false, ignoreModules: [], transitionInterval: 10000},
-            bottom_bar: {enabled: false, ignoreModules: [], transitionInterval: 10000},
+            mode: 'global', //global || positional || slides
+            top_bar: {enabled: false, ignoreModules: [], overrideTransitionInterval: 10000},
+            top_left: {enabled: false, ignoreModules: [], overrideTransitionInterval: 10000},
+            top_center: {enabled: false, ignoreModules: [], overrideTransitionInterval: 10000},
+            top_right: {enabled: false, ignoreModules: [], overrideTransitionInterval: 10000},
+            upper_third: {enabled: false, ignoreModules: [], overrideTransitionInterval: 10000},
+            middle_center: {enabled: false, ignoreModules: [], overrideTransitionInterval: 10000},
+            lower_third: {enabled: false, ignoreModules: [], overrideTransitionInterval: 10000},
+            bottom_left: {enabled: false, ignoreModules: [], overrideTransitionInterval: 10000},
+            bottom_center: {enabled: false, ignoreModules: [], overrideTransitionInterval: 10000},
+            bottom_right: {enabled: false, ignoreModules: [], overrideTransitionInterval: 10000},
+            bottom_bar: {enabled: false, ignoreModules: [], overrideTransitionInterval: 10000},
             slides: [
                 []
             ]
@@ -28,7 +28,7 @@
             if (notification === 'DOM_OBJECTS_CREATED') {
                 // Initially, all modules are hidden except the first and any ignored modules
                 // We start by getting a list of all of the modules in the transition cycle
-                if (this.config.mode === 'global') {
+                if ((this.config.mode === 'global') || (this.config.mode === 'slides')) {
                     this.setUpTransitionTimers(null);
                 } else {
                     for (position = 0; position < positions.length; position += 1) {
@@ -43,63 +43,45 @@
         setUpTransitionTimers: function (positionIndex) {
             var i, modules, timer = this.config.transitionInterval;
             modules = MM.getModules().exceptModule(this).filter(function (module) {
-                if ((this.config.mode === 'global') || (this.config.mode==='slides')) {
+                if ((this.config.mode === 'global') || (this.config.mode === 'slides')) {
                     return this.config.ignoreModules.indexOf(module.name) === -1;
                 }
                 return ((this.config[positionIndex].ignoreModules.indexOf(module.name) === -1) && (module.data.position === positionIndex));
             }, this);
             modules.currentIndex = 0;
 
-            if (this.config.mode==='slides') {
+            if (this.config.mode === 'slides') {
                 modules.slides = this.config.slides;
-                this.moduleTransitionFixed.bind(modules);
-                this.transitionTimer = setInterval(this.moduleTransitionFixed.bind(modules), timer)
             } else {
                 for (i = 1; i < modules.length; i += 1) {
                     modules[i].hide();
                 }
-                if ((this.config[positionIndex].transitionInterval !== 'undefined') && (this.config[positionIndex].transitionInterval > 0)) {
-                    timer = this.config[positionIndex].transitionInterval;
+                if ((this.config[positionIndex].overrideTransitionInterval !== 'undefined') && (this.config[positionIndex].overrideTransitionInterval > 0)) {
+                    timer = this.config[positionIndex].overrideTransitionInterval;
                 }
-                // We set a timer to cause the page transitions
-                this.transitionTimer = setInterval(this.moduleTransitionRotating.bind(modules), timer);
             }
+            // We set a timer to cause the page transitions
+            this.transitionTimer = setInterval(this.moduleTransition.bind(modules), timer);
         },
 
-        moduleTransitionRotating: function () {
-            var i;
-
+        moduleTransition: function () {
+            var i, resetCurrentIndex = this.length;
+            if (this.slides !== 'undefined') {
+                resetCurrentIndex = this.slides.length;
+            }
             // Update the current index
             this.currentIndex += 1;
-            if (this.currentIndex >= this.length) {
+            if (this.currentIndex >= resetCurrentIndex) {
                 this.currentIndex = 0;
             }
 
             for (i = 0; i < this.length; i += 1) {
                 // There is currently no easy way to discover whether a module is ALREADY shown/hidden
                 // In testing, calling show/hide twice seems to cause no issues
-                if (i === this.currentIndex) {
+                if (((this.slides === 'undefined') && (i === this.currentIndex)) || ((this.slides !== 'undefined') && (this.slides[this.currentIndex].indexOf(this[i].name) !== -1))) {
                     this[i].show(1500);
                 } else {
                     this[i].hide(0);
-                }
-            }
-        },
-
-        moduleTransitionFixed: function () {
-            var i;
-
-            // Update the current index
-            this.currentIndex += 1;
-            if (this.currentIndex >= this.slides.length) {
-                this.currentIndex = 0;
-            }
-
-            for (i = 0; i < this.length; i += 1) {
-                if (this.slides[this.currentIndex].indexOf(this[i].name) === -1) {
-                    this[i].hide(0);
-                } else {
-                    this[i].show(1500);
                 }
             }
         }
